84e010acefbc2e10c138ac35c1053ef0ef26c030
vinpap2008S <mirzoevsergej881@gmail.com>
2024-01-16 19:20:45 +0300

    Дз на 16.01

--- Homework/Homework.cpp
+++ Homework/Homework.cpp
@@ -1,6 +1,35 @@
 ﻿#include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+    string task;
+    cout << "Введите задачу: ";
+    cin >> task;
+
+    int pos = 0;
+    int sum = 0;
+
+    while (pos < task.length()) {
+        int start = task.find_first_of("0123456789", pos);
+        if (start == -1) {
+            break;
+        }
+
+        int end = task.find_first_not_of("0123456789", start);
+        if (end == -1) {
+            end = task.length();
+        }
+
+        string number_str = task.substr(start, end - start);
+        int number = stoi(number_str);
+        sum += number;
+
+        pos = end;
+    }
+
+    cout << "Ответ: " << sum << endl;
+
+    return 0;
 }
