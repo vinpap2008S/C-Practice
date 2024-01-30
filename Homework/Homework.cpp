332c2053a26f4f8457a25f794188641afabab469
vinpap2008S <mirzoevsergej881@gmail.com>
2024-01-30 19:17:11 +0300

    Читать слова или числа из потока ввода до точки. Сохранить в list

--- Homework/Homework.cpp
+++ Homework/Homework.cpp
@@ -1,47 +1,25 @@
 ﻿#include <iostream>
+#include <list>
 #include <string>
-#include <cstdlib>
-#include <ctime>
+
 using namespace std;
-class Monster {
-private:
-    string name;
-    string weapon1;
-    string weapon2;
-
-public:
-    Monster(string n, string w1, string w2) : name(n), weapon1(w1), weapon2(w2) {}
-
-    void attack(Monster& other) {
-        cout << name << " attacks with " << weapon1 << " and " << weapon2 << endl;
-        // реализация атаки
-    }
-
-    bool isDefeated() {
-        return weapon1.empty() && weapon2.empty();
-    }
-};
 
 int main() {
-    srand(time(0));
+    setlocale(LC_ALL, "ru");
+    list<string> wordsOrNumbers;
 
-    Monster monster1("Monster1", "Axe", "Sword");
-    Monster monster2("Monster2", "Club", "Bow");
-
-    Monster* attacker = &monster1;
-    Monster* defender = &monster2;
-
-    while (!monster1.isDefeated() && !monster2.isDefeated()) {
-        attacker->attack(*defender);
-        swap(attacker, defender);
+    string input;
+    cout << "Введите слова или числа (введите точку для завершения): ";
+    while (cin >> input and input != ".")
+    {
+        wordsOrNumbers.push_back(input);
     }
 
-    if (monster1.isDefeated()) {
-        cout << "Monster2 wins!" << endl;
+    cout << "Список значений до точки" << endl;
+    for (const string& item : wordsOrNumbers) {
+        cout << item << endl;
     }
-    else {
-        cout << "Monster1 wins!" << endl;
-    }
+
 
     return 0;
 }
