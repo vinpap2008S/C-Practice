e226ba91e3e63b5c1d14a8b0b6db4ab96254c868
vinpap2008S <mirzoevsergej881@gmail.com>
2024-02-14 15:37:39 +0300

    класс "треугольник"

--- Homework/algebra.h
+++ Homework/algebra.h
@@ -1,79 +1,55 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <unordered_map>
-#include <vector>
-#include <ctime>
+#include <cmath>
 
-class Dictionary {
+using namespace std;
+
+class Triangle {
 private:
-    std::unordered_map<std::string, std::vector<std::string>> words;
+    double side1;
+    double side2;
+    double side3;
 
 public:
-    void addWord(const std::string& word, const std::vector<std::string>& synonyms) {
-        words[word] = synonyms;
+    Triangle(double s1, double s2, double s3) : side1(s1), side2(s2), side3(s3) {
+        if (!isValid()) {
+            throw invalid_argument("Triangle cannot be created. Invalid sides.");
+        }
     }
 
-    std::vector<std::string> translate(const std::string& word) {
-        if (words.find(word) != words.end()) {
-            return words[word];
-        }
-        return { "Translation not found" };
+    double getPerimeter() const {
+        return side1 + side2 + side3;
     }
 
-    void randomTest(int N) {
-        std::vector<std::string> testedWords;
-        srand(time(0));
+    double getArea() const {
+        double p = getPerimeter() / 2;
+        return sqrt(p * (p - side1) * (p - side2) * (p - side3));
+    }
 
-        std::ofstream testFile("test_results.txt");
-        if (testFile.is_open()) {
-            testFile << "Tested Words:\n";
-
-            for (int i = 0; i < N; ++i) {
-                int randomIndex = rand() % words.size();
-                auto it = std::next(words.begin(), randomIndex);
-                std::string word = it->first;
-
-                testFile << word << "\n";
-                testedWords.push_back(word);
-            }
-
-            testFile << "\nResults:\n";
-            for (const auto& word : testedWords) {
-                testFile << "Word: " << word << "\n";
-                testFile << "Translation: ";
-                for (const auto& synonym : translate(word)) {
-                    testFile << synonym << ", ";
-                }
-                testFile << "\n\n";
-            }
-
-            testFile.close();
-        }
-        else {
-            std::cerr << "Unable to open file for writing." << std::endl;
-        }
+    bool isValid() const {
+        return (side1 + side2 > side3) && (side2 + side3 > side1) && (side1 + side3 > side2);
     }
 };
 
+bool compareByPerimeter(const Triangle& t1, const Triangle& t2) {
+    return t1.getPerimeter() < t2.getPerimeter();
+}
+
+bool compareByArea(const Triangle& t1, const Triangle& t2) {
+    return t1.getArea() < t2.getArea();
+}
+
 int main() {
-    Dictionary dict;
+    try {
+        Triangle t1(3, 4, 5);
+        Triangle t2(5, 12, 13);
+        Triangle t3(8, 15, 17);
 
-    // Добавление слов и синонимов
-    dict.addWord("apple", { "яблоко", "яблоко" });
-    dict.addWord("house", { "дом", "жилище" });
-    dict.addWord("car", { "машина", "автомобиль" });
-
-    // Перевод слова
-    std::vector<std::string> translation = dict.translate("apple");
-    std::cout << "Translation of 'apple': ";
-    for (const auto& synonym : translation) {
-        std::cout << synonym << ", ";
+        cout << "Perimeter Comparison: " << compareByPerimeter(t1, t2) << endl;
+        cout << "Area Comparison: " << compareByArea(t1, t2) << endl;
     }
-    std::cout << std::endl;
-
-    // Проведение теста и запись результатов в файл
-    dict.randomTest(3);
+    catch (const exception& e) {
+        cout << "Exception: " << e.what() << endl;
+    }
 
     return 0;
 }
